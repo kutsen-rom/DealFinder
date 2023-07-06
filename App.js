b06632caf2d845b83ce2ba06dbf6fa6e4bc02d20
kutsen-rom <62949847+kutsen-rom@users.noreply.github.com>
2023-07-06 14:01:43 +0300

    Implement touchables

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, StyleSheet, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+} from "react-native";
+// import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
@@ -8,10 +15,19 @@ export default function App() {
       {/* <StatusBar style="auto" /> */}
       {/* I've looked through bunch of apps and found out that even Uber doesn't have the StatusBar so I decided to remove it at least for now */}
       <Text>DealFinder</Text>
-      <Image
-        loadingIndicatorSource={require('./assets/image.png')}
-        source={{ width: 200, height: 300 ,uri: "https://picsum.photos/200/300" }}
-      />
+      <TouchableHighlight onPress={() => console.log(123)}>
+        <Image
+          loadingIndicatorSource={require("./assets/image.png")}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
+      <TouchableNativeFeedback>
+        <View style={{width: 100, height: 50, backgroundColor: 'lightblue'}}></View>
+      </TouchableNativeFeedback>
     </View>
   );
 }
