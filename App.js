910ac15f80dc66796c2bf06624381be9d1479085
kutsen-rom <62949847+kutsen-rom@users.noreply.github.com>
2023-07-06 14:28:15 +0300

    Make a custom alert

--- App.js
+++ App.js
@@ -1,33 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  Image,
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight,
-  TouchableNativeFeedback,
-} from "react-native";
-// import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, View, Button, Alert } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <StatusBar style="auto" /> */}
-      {/* I've looked through bunch of apps and found out that even Uber doesn't have the StatusBar so I decided to remove it at least for now */}
-      <Text>DealFinder</Text>
-      <TouchableHighlight onPress={() => console.log(123)}>
-        <Image
-          loadingIndicatorSource={require("./assets/image.png")}
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableHighlight>
-      <TouchableNativeFeedback>
-        <View style={{width: 100, height: 50, backgroundColor: 'lightblue'}}></View>
-      </TouchableNativeFeedback>
+      <Button
+        onPress={() =>
+          Alert.alert("Alert title", "Alert message", [
+            { text: "Yes", onPress: () => console.log("Yes") },
+            { text: "No", onPress: () => console.log("No") },
+          ])
+        }
+        title="Press me"
+      />
     </View>
   );
 }
